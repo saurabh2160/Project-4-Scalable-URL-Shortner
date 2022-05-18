bfa3a7382a25b736ba50e6bd8a20a3253386e655
saurabh2160 <saurabhmanohar90@gmail.com>
2022-05-18 13:25:55 +0530

    new chnage

--- src/urlcontroller/urlController.js
+++ src/urlcontroller/urlController.js
@@ -2,6 +2,8 @@ const urlModel = require('../modules/urlModel')
 const validUrl = require('valid-url')
 const shortid = require('shortid');
 
+
+//=======================================================[CREATE SHORT URL]=============================================================
 const urlshortner = async (req, res) => {
     try {
         let inurl = req.body
@@ -12,7 +14,7 @@ const urlshortner = async (req, res) => {
         //validating url
         if (!validUrl.isUri(originalUrl)) return res.status(400).send({ status: false, message: "Enter a valid url" })
         //creating short url
-        let code=shortid.generate().toLowerCase()
+        let code = shortid.generate().toLowerCase()
         let short = "http://localhost:3000/" + code
         let output = {
             longUrl: originalUrl,
@@ -20,11 +22,14 @@ const urlshortner = async (req, res) => {
             urlCode: code
         }
         let search = await urlModel.findOne({ shortUrl: short, urlCode: code })
-        //console.log(search)
         //searching for Urlcode and shorturl in DB
         if (search) {
             if (search.urlCode == code) return res.status(400).send({ status: false, message: "urlcode  already exits" })
             if (search.shortUrl == short) return res.status(400).send({ status: false, message: "Shorturl already exits" })
+        }
+        let findlongurl = await urlModel.findOne({ longUrl: originalUrl }).select({ __v: 0, _id: 0, createdAt: 0, updatedAt: 0 })
+        if (findlongurl) {
+            return res.status(201).send({ status: true, data: findlongurl })
         }
         const result = await urlModel.create(output)
         if (result) {
@@ -35,30 +40,22 @@ const urlshortner = async (req, res) => {
         res.status(500).send({ status: false, message: er.message })
     }
 }
-
-
-const getUrl = async function (req,res) {
+//======================================================[GET URL API]===========================================================================
+const getUrl = async function (req, res) {
     try {
         let code = req.params.urlCode;
-        const check = await urlModel.findOne({urlCode: code})
-         res.writeHead(301, {
-            "Location": check.longUrl
-        });
+
+        const check = await urlModel.findOne({ urlCode: code })
+        // res.writeHead(301, {
+        //     "Location": check.longUrl
+        // });
+        res.status(301).send({status:false,data:check.longUrl})
         res.end()
-        //res.status(301)send({status:true,data:check.longUrl})
     }
     catch (er) {
         res.status(500).send({ status: false, message: er.message })
     }
 }
-// function for urlshortner
-// const urlconverter = (longURL) => {
-//     let obj = {}
-//     let shortURL = "http://localhost:3000/" + longURL.replace(/[^a-z]/g, '').slice(-4);
-//     if (!obj[shortURL]) {
-//         obj[shortURL] = longURL;
-//     };
-//     return shortURL
-// }
 
-module.exports = { urlshortner , getUrl}
+
+module.exports = { urlshortner, getUrl }
